c5850173d7d86bba980f3e6b4925bc0e1ffbd6fb
lakintosh <92750553+lakintosh@users.noreply.github.com>
2021-11-26 13:19:46 +0100

    Add choose operation and provide numbers

---
+++ main.py
@@ -0,0 +1,4 @@
+operation = input('Podaj działanie, posługując się odpowiednią liczbą: 1.Dodawanie, 2.Odejmowanie, 3.Mnożenie, 4.Dzielenie: ')
+print("Następnie podaj liczby: ")
+num1 = input()
+num2 = input()
